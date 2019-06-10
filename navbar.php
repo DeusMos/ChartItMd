408af9d74ef9281ff2150db0754bb924036f882f
jwood <jamesewood87@gmail.com>
2019-06-10 16:11:16 -0700

    I added the script for the demo and fixed a navabar bug.

--- navbar.php
+++ navbar.php
@@ -6,13 +6,13 @@
     <ul>
         <li><a href="PatientLookup.php">Open Patient</a></li>
         <li><a href="index.php"> Patient Summery</a></li>
-        <li hidden><a href="Orders.php">Orders</a> </lihidden>
+        <li hidden><a href="Orders.php">Orders</a> </li>
         <li><a href="Medications.php">Medications</a> </li>
         <li hidden><a href="LabReports.php">Lab Reports</a> </li>
         <li hidden><a href="Admission.php">Admission</a> </li>
         <li><a href="DoctorsNotes.php">Doctor's Notes</a> </li>
         <li><a href="NursesNotes.php">Nurses's Notes</a> </li>
-        <li hidden><a href="ConsentForms.php">Consent Forms</a> </lihidden>
+        <li hidden><a href="ConsentForms.php">Consent Forms</a> </li>
         <li><a href="settings.php">Settings</a></li>
         <li><a href="about.php"> About</a></li>
         <li><a href="patientContact.php"> Patient Contact</a></li>
